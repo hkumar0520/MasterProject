adc0ab41fc00486c0895114f525877046ccc4dcb
hkumar0520 <53545303+hkumar0520@users.noreply.github.com>
2022-07-04 22:24:54 +0530

    Backend Folder Sctrucutre 24 minutes

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts',postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+
+// const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+//mongodb+srv://javascriptmastery:1234@cluster0.7awa0.mongodb.net/?retryWrites=true&w=majority
+// const CONNECTION_URL = 'mongodb://localhost:27017/Project';
+const CONNECTION_URL = 'mongodb+srv://javascriptmastery:1234@cluster0.7awa0.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+// do not get any warning in console
+// depriciated
+// mongoose.set('useFindAndModify', false);
