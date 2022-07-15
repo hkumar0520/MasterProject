d7c38bdd5d00a0de0ab4c5610d1c860c05bc7177
hkumar0520 <53545303+hkumar0520@users.noreply.github.com>
2022-07-15 20:39:55 +0530

    FIRST VIDEO COMPLETE

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,19 +1,23 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts/';
+const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => {
-    // console.log(newPost);
-    // console.log('hiapitwo')
-// axios.post(url, newPost)
-// .then(response => console.log(response))
+export const createPost = (newPost) =>  axios.post(url, newPost);
+
+
+
+
+// export const createPost = async (newPost) => {
+//         axios.post(url, newPost)
+//         .then(response => console.log('hires '+response))
 //         .catch(error => {
 //             console.error('There was an error!', error);
 //         });
-axios.post(url, newPost);
-// console.log("hi")
-};
+
+//         axios.post(url, newPost);
+//         const response = await fetch(url, {method: 'POST'}, newPost);
+// };
     
 
 
