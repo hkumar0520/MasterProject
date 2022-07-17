efbf73ad035b0cd3947f14aa876ed99f14cf7ebb
hkumar0520 <53545303+hkumar0520@users.noreply.github.com>
2022-07-17 07:32:25 +0530

    Backend Comments updated

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,4 +1,6 @@
+// direct does not require name should be same, can use import expressss from 'express'
 import express from 'express'
+// within {} name should be same from where we getting those functions
 import {getPosts, createPost} from '../controllers/posts.js'
 
 const router = express.Router();
