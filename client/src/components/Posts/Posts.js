53fcfcf126d61012d2d826eb99760b66dc08426a
hkumar0520 <53545303+hkumar0520@users.noreply.github.com>
2022-07-24 07:28:42 +0530

    Posts UI 12:40 minutes sec

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import { Grid, CircularProgress } from '@material-ui/core';
 import Post from './Post/Post.js';
 import useStyles from './styles';
 
@@ -9,12 +9,18 @@ const Posts = () => {
     const classes = useStyles();
 
     console.log(posts);
+
+    console.log(posts);
     return(
-        <>
-            <h1>POSTS</h1>
-            <Post/>
-            <Post/>
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+                {posts.map((post) => (
+                    <Grid key = {post._id} item xs={12} sm={6}>
+                        <Post post={post}/>
+                    </Grid>
+                ))}
+            </Grid>
+        )
         
     );
 }
