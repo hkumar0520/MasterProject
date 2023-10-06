0b89e74672eb2ed553449814113bf665fcd5f1f1
hkumar0520 <53545303+hkumar0520@users.noreply.github.com>
2023-10-06 21:02:09 +0530

    Update functionality implementation
    update functionality implementation

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -8,22 +8,52 @@ export const getPosts = () => async (dispatch) => {
     // return action;
 
     try{
+        console.log('getPosts1');
         const { data }  = await api.fetchPosts();
 
+        console.log('getPosts2');
         dispatch({type: 'FETCH_ALL', payload: data})
     } catch (error) {
-        console.log(error.message)
+        console.log(error)
+        // console.log(error.message)
     }
 }
 
 
 export const createPost = (post) => async (dispatch) => {
     try{
+        console.log('createPost1');
         const { data } = await api.createPost(post);
         
+        console.log('createPost2');
         dispatch({ type: 'CREATE', payload: data});
 
     } catch (error) {
-        console.log(error.message)
+        console.log(error)
+        // console.log(error.message)
     }
 }
+
+export const updatePost = (id, post) => async (dispatch) =>{
+    try{
+        console.log('updatepost1');
+        const {data} = await api.updatePost(id, post);
+
+        console.log('updatepost2');
+
+        dispatch({type: 'UPDATE', payload: data})
+    } catch(error){
+        console.log(error.message)
+        // console.log(error.message);
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 // direct does not require name should be same, can use import expressss from 'express'
 import express from 'express'
 // within {} name should be same from where we getting those functions
-import {getPosts, createPost} from '../controllers/posts.js'
+import {getPosts, createPost, updatePost} from '../controllers/posts.js'
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
+router.patch('/:id', updatePost);
 
 export default router;
