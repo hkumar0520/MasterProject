f152f7ff24ec68bde6a0d200a2e4c4efd1e35486
hkumar0520 <53545303+hkumar0520@users.noreply.github.com>
2023-10-08 19:07:03 +0530

    Layout 24 minutes
    layout completed 24 minutes

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
-import { useDispatch } from 'react-redux';
+import React  from 'react';
+import { Container} from '@material-ui/core'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts.js';
-import Form from './components/Form/Form.js';
-import useStyles from './styles';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+import Navbar from './components/Navbar/Navbar';
 
-import memories from './images/memories.png';
 
 const App = () =>{
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(()=>{
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
 
     return(
-        <Container maxwidth = "lg">
-            <AppBar className={classes.appBar} position='static' color='inherit'>
-                {/* better fonts for typography */}
-                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
-                <img className={classes.image} src={memories} alt='memories' height='60' ></img>
-                {/* grow for animation */}
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justifyContent = "space-between" alignItems='stretch' spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId = {setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form setCurrentId = {setCurrentId} currentId={currentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <BrowserRouter>
+            <Container maxwidth = "lg">
+                <Navbar/>
+                <Routes>
+                    <Route exact path="/"  element={<Home />} />
+                    <Route exact path="/auth"  element={<Auth />} />
+                </Routes>
+            </Container>
+        </BrowserRouter>
     );
 }
 
---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Auth = () => {
+    return (
+        <div>
+            Auth
+        </div>
+    );
+}
+
+export default Auth;
+
+
+
